3ea0f9075d73a4cd741da82e0ea5048b3cc7006e
Despina N.A. <despinannebs22@gmail.com>
2026-09-24 22:57:21 +0100

    created the article application

--- article/models.py
+++ article/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class EventHighlight(model.Models):
+    title = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    story = models.TextField()
+    event_date = models.DateField()
+    display_order = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class FanSubmission(model.Models):
+    title = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    story = models.TextField()
+    event_date = models.DateField()
+    display_order = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title

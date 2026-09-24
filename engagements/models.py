032e255da3b1f5dff617ec59a0ebe8fd562f74f2
Samuel <lilnature042@gmail.com>
2026-09-24 16:34:38 +0100

    created the characters app

--- engagements/models.py
+++ engagements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from accounts import User
 
 # Create your models here.
+class Feedback(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="feedback")
+  type = models.CharField(max_length=20, choices=[("bug", "Bug"), ("suggestion", "Suggestion"), ("query", "Query")])
+  message = models.TextField()
+  status = models.CharField(max_length=20, choices=[("new", "New"), ("in_review", "In_Review"),("resolved", "Resolved")], default="new") 
+  admin_response = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)

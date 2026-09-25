356a5715fcfa769ae1d7cbbe69be827081271a35
Chigozie Emmanuel <stompiddo3@gmail.com>
2026-09-25 14:04:39 +0100

    Updated admin.pyfor both accounts and media_centre

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,92 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Profile
+
+
+# Custom User Admin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    # Fields displayed in the user list
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "email_verified",
+        "is_active",
+        "created_at",
+    )
+
+    # Filters available in the sidebar
+    list_filter = (
+        "role",
+        "email_verified",
+        "is_active",
+    )
+
+    # Fields that can be searched
+    search_fields = (
+        "username",
+        "email",
+    )
+
+    # Show newest users first
+    ordering = ("-created_at",)
+
+    # Additional fields when viewing/editing a user
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Fan Hub Information",
+            {
+                "fields": (
+                    "role",
+                    "email_verified",
+                    "created_at",
+                )
+            },
+        ),
+    )
+
+    # Additional fields when creating a user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Fan Hub Information",
+            {
+                "fields": (
+                    "email",
+                    "role",
+                    "email_verified",
+                )
+            },
+        ),
+    )
+
+
+# User Profile Admin
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    # Fields displayed in the profile list
+    list_display = (
+        "user",
+        "theme",
+        "font_size",
+    )
+
+    # Filters
+    list_filter = (
+        "theme",
+        "font_size",
+    )
+
+    # Search users by username or email
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
+
+    # Makes selecting multiple favorite categories easier
+    filter_horizontal = (
+        "favorite_categories",
+    )
--- media_centre/admin.py
+++ media_centre/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rating
+
+
+# Register the Rating model
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+
+    # Fields displayed in the rating list
+    list_display = (
+        "user",
+        "media",
+        "rating",
+        "created_at",
+        "updated_at",
+    )
+
+    # Filters available in the sidebar
+    list_filter = (
+        "rating",
+        "created_at",
+    )
+
+    # Search ratings by username, email, or media title
+    search_fields = (
+        "user__username",
+        "user__email",
+        "media__title",
+    )
+
+    # Show newest ratings first
+    ordering = ("-created_at",)
